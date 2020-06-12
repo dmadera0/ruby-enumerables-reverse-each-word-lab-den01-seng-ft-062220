815a9ee5acac2e3902fc1f4778712acd04f85a84
Daniel Madera <dmadera0@gmail.com>
2020-06-12 14:29:50 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,3 @@
 def reverse_each_word(sentence)
-original_array = sentence.split(" ")
-  return_array = []
-  original_array.each do|sentence|
-    return_array << sentence.reverse
-  end
-  return_array.join(" ")
+sentence.split.map {|word| word.reverse}.join(" ")
 end
