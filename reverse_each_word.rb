3ccc4ef43826d2393c91c687c19b91360edc6d3d
Daniel Madera <dmadera0@gmail.com>
2020-06-12 14:27:23 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(sentence)
-original_array = string.split(" ")
+original_array = sentence.split(" ")
   return_array = []
-  original_array.each do|string|
-    return_array << string.reverse
+  original_array.each do|sentence|
+    return_array << sentence.reverse
   end
   return_array.join(" ")
 end
